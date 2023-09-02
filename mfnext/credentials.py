2acd618f509cfd3365f2333b75ba83e7e03f1413
Soab Mahmud Syfuddhin <108150913+Soab42@users.noreply.github.com>
2023-09-02 19:43:55 +0600

    Update credentials.py

--- mfnext/credentials.py
+++ mfnext/credentials.py
@@ -1,15 +1,9 @@
 # credentials.py
 
 usernames = [
-    "sabuzbag", "badda", "jatrabari040", "konapara", "nayabazar1",
-    "noyabazar2", "noyabazar3", "Nayabazar-2", "azampur", "dkhan063",
-    "uttara103", "panchaboti", "postogola123", "fotulla-2", "samoli",
-    "lalbag", "dhanmondi"
+    //user name
 ]
 
 passwords = [
-    "01877703026", "01712391313", "pmk1111111", "pmkkonapara", "01709914012",
-    "pmk123456789", "noyabazar090", "21993232", "s01877703010", "01877703063",
-    "Uttara@103", "pmkpanchabati", "pmk123456789", "01877703073", "01709914053pmk",
-    "044pmklalbag", "pmkdhanmondi020@"
+        // password
 ]
